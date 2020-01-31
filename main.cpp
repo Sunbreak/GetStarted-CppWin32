db0b9268905f9e97e8edb7f3365d7c7b2b2e7d23
wangkun <wangkun@woodemi.com>
2020-01-31 16:12:12 +0800

    Initial commit
    Signed-off-by: wangkun <wangkun@woodemi.com>

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <Windows.h>
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow)
+{
+    auto CLASS_NAME{ L"Sample Window Class" };
+
+    WNDCLASS wc = {};
+    wc.lpfnWndProc = DefWindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+
+    RegisterClass(&wc);
+
+    HWND hwnd = CreateWindowEx(
+        0,
+        CLASS_NAME,
+        L"GetStarted-CppWin32",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+        nullptr,
+        nullptr,
+        hInstance,
+        nullptr
+    );
+
+    if (hwnd == nullptr)
+        return 0;
+
+    ShowWindow(hwnd, nCmdShow);
+
+    return 0;
+}
