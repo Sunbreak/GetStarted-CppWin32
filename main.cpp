4aec958d5ecbe943dfba12f2fa57df57f180b43d
wangkun <wangkun@woodemi.com>
2020-01-31 22:49:21 +0800

    Handle `WM_PAINT` in WndProc
    Signed-off-by: wangkun <wangkun@woodemi.com>

--- main.cpp
+++ main.cpp
@@ -41,19 +41,27 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow
     return 0;
 }
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
     switch (uMsg)
     {
     case WM_SIZE:
-        {
-            int width = LOWORD(lParam);
-            int height = HIWORD(lParam);
-            OutputDebugString((L"width " + std::to_wstring(width) + L", height " + std::to_wstring(height) + L"\n").c_str());
-        }
+    {
+        int width = LOWORD(lParam);
+        int height = HIWORD(lParam);
+        OutputDebugString((L"width " + std::to_wstring(width) + L", height " + std::to_wstring(height) + L"\n").c_str());
         break;
+    }
+    case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hwnd, &ps);
+        FillRect(hdc, &ps.rcPaint, (HBRUSH) (COLOR_WINDOW + 1));
+        EndPaint(hwnd, &ps);
+        return 0;
+    }
     default:
         break;
     }
-    return DefWindowProc(hWnd, uMsg, wParam, lParam);
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
